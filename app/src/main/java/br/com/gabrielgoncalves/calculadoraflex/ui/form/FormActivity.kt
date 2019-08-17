d44f576771d9cde89d0b2e72e5d4e807eceab7c9
Logon Aluno <logonaluno@local.com>
2019-08-17 09:37:47 -0300

    Initial commit

---
+++ app/src/main/java/br/com/gabrielgoncalves/calculadoraflex/ui/form/FormActivity.kt
@@ -0,0 +1,13 @@
+package br.com.gabrielgoncalves.calculadoraflex.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.gabrielgoncalves.calculadoraflex.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ app/src/main/java/br/com/gabrielgoncalves/calculadoraflex/ui/result/ResultActivity.kt
@@ -0,0 +1,13 @@
+package br.com.gabrielgoncalves.calculadoraflex.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.gabrielgoncalves.calculadoraflex.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
